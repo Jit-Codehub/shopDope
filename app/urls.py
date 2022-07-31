bb43e3f6f4b6749618bc4220587638e49f98e472
Jitender Kumar <jkcodingjk@gmail.com>
2022-07-31 18:20:23 +0530

    password change functionality added

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from app import views
 from django.contrib.auth import views as auth_views
-from .forms import LoginForm
+from .forms import LoginForm, MyPasswordChangeForm
 urlpatterns = [
     path('', views.ProductView.as_view(), name='home'),
     path('product-detail/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
@@ -10,7 +10,6 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('address/', views.address, name='address'),
     path('orders/', views.orders, name='orders'),
-    path('changepassword/', views.change_password, name='changepassword'),
     path('mobile/', views.mobile, name='mobile'),
     path('mobile/<data>/', views.mobile, name="mobiledata"),
     path('checkout/', views.checkout, name='checkout'),
@@ -18,5 +17,9 @@ urlpatterns = [
     path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='app/login.html', authentication_form=LoginForm), name='login'),
     path("logout/", auth_views.LogoutView.as_view(next_page='login'), name="logout"),
+
+    path('passwordchange/', auth_views.PasswordChangeView.as_view(template_name="app/passwordchange.html", form_class=MyPasswordChangeForm, success_url='/passwordchangedone/'), name='passwordchange'),
+
+    path('passwordchangedone/', auth_views.PasswordChangeDoneView.as_view(template_name="app/passwordchangedone.html"), name='passwordchangedone'),
     
 ]
