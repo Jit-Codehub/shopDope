fb36c666d8454ad9128fed16ac6e394f0bb09df3
Jitender Kumar <jkcodingjk@gmail.com>
2022-08-02 19:52:20 +0530

    authentication part is finished

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
+import email
 import imp
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField, PasswordChangeForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UsernameField, PasswordChangeForm, PasswordResetForm, SetPasswordForm
 from django.contrib.auth.models import User
 from django.utils.translation import gettext, gettext_lazy as _
 from django.contrib.auth import password_validation
@@ -24,6 +25,15 @@ class LoginForm(AuthenticationForm):
 class MyPasswordChangeForm(PasswordChangeForm):
     old_password = forms.CharField(label=_("Old Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'current-password', 'autofocus':True, 'class': 'form-control'}))
 
+    new_password1 = forms.CharField(label=_("New Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'new-password', 'class': 'form-control'}), help_text=password_validation.password_validators_help_text_html())
+
+    new_password2 = forms.CharField(label=_("Confirm New Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'new-password', 'class': 'form-control'}))
+
+class MyPasswordResetForm(PasswordResetForm):
+    email = forms.EmailField(label=_("Email"), max_length=254, widget=forms.EmailInput(attrs={'autocomplete':'email', 'class': 'form-control'}))
+
+
+class MySetPasswordForm(SetPasswordForm):
     new_password1 = forms.CharField(label=_("New Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'new-password', 'class': 'form-control'}), help_text=password_validation.password_validators_help_text_html())
 
     new_password2 = forms.CharField(label=_("Confirm New Password"), strip=False, widget=forms.PasswordInput(attrs={'autocomplete':'new-password', 'class': 'form-control'}))
